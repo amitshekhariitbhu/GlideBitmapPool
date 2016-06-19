0260e1aad346ae9358a23d713583dae7a7fda7ce
AMIT SHEKHAR <amit.shekhar.iitbhu@gmail.com>
2016-06-19 07:53:13 +0530

    calculateInSampleSize added

--- glide-bitmap-pool/src/main/java/com/glidebitmappool/GlideBitmap.java
+++ glide-bitmap-pool/src/main/java/com/glidebitmappool/GlideBitmap.java
@@ -81,6 +81,30 @@ public class GlideBitmap {
                 && targetOptions.inSampleSize == 1;
     }
 
+
+    private static int calculateInSampleSize(
+            BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) > reqHeight
+                    && (halfWidth / inSampleSize) > reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
+    }
+
     /**
      * A helper function to return the byte usage per pixel of a bitmap based on its configuration.
      */
